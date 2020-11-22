7e572d083aeed1e6c623613d7f821dd605b61c8d
ArthurMargarian <Artur.Margaryan@volo.global>
2020-11-22 19:17:26 +0400

    inital commit,nodeJS part

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/db');
+
+// Connect to db
+mongoose.connect(config.database, {useUnifiedTopology: true, useNewUrlParser: true});
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to ${config.database} db`);
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Db Error: ${err}`);
+})
+
+
+const app = express();
+
+const users = require('./routes/users');
+
+// Port Number
+const port = 3000;
+
+// // Cors Middleware
+app.use(cors());
+
+// Set Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// // Body Parser Middleware
+app.use(bodyParser.json());
+app.use('/users', users);
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+
+
+// Index Route
+app.get('/', (req, res) => {
+    res.send('invalid endpoint');
+})
+
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
+});
---
+++ config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database: "mongodb+srv://user:pass@mean-db.cqwkl.mongodb.net/test",
+    secret: "yourSecret"
+}
+// mongodb+srv://user:pass@mean-db.cqwkl.mongodb.net/test?authSource=admin&replicaSet=atlas-k6hub9-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
